e3ba892c68ea957858011293d7032ae10c373752
BlueCalculator <85308130+BlueCalculator@users.noreply.github.com>
2022-11-04 22:06:17 -0400

    Add files via upload

---
+++ mostPointlessGameEver/main.js
@@ -0,0 +1,20 @@
+let i = 0
+
+console.log('█▀█ █▀█ █ █▄░█ ▀█▀ █░░ █▀▀ █▀ █▀   █▀▀ ▄▀█ █▀▄▀█ █▀▀\n█▀▀ █▄█ █ █░▀█ ░█░ █▄▄ ██▄ ▄█ ▄█   █▄█ █▀█ █░▀░█ ██▄\n')
+
+document.getElementById("text").innerHTML = i;
+function numbers(){
+    console.log('You have clicked a button')
+    i++;
+    document.getElementById("text").innerHTML = i;
+
+    if(i >= 10){
+        window.open('https://www.google.com/', '_self');
+    }
+}
+
+
+
+
+
+
